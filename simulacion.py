1d8257b44b12350180e2a3edea1399f14bfbf7b5
Santiago Pesce <santi.pesce12@gmail.com>
2025-06-10 18:11:12 +0000

    Modifico nombres de variables y agrego una funcion derivadas, calcular flujo del ejer 5 y metodo Runge-Kutta

--- simulacion.py
+++ simulacion.py
@@ -7,25 +7,43 @@ coef_transferencia = 0.5       # K1: coeficiente de transferencia entre tanques
 coef_descarga = 1.0            # K2: coeficiente de descarga del segundo tanque
 gravedad = 9.8                 # Aceleración gravitatoria (m/s²)
 paso_tiempo = 0.1              # Paso de integración (s)
-tiempo_total = 100             # Duración total de la simulación (s)
+tiempo_total = 2000             # Duración total de la simulación (s)
 
 # --- Vector de tiempo ---
 tiempo = np.arange(0, tiempo_total + paso_tiempo, paso_tiempo)
 
 # --- Condiciones iniciales ---
-altura1_const = [1000.0]       # Altura inicial en tanque 1 (entrada constante)
-altura2_const = [0.0]          # Altura inicial en tanque 2 (entrada constante)
+# Entrada constante
+altura1_const_euler = [1000.0]  # Altura inicial en tanque 1 (Euler, entrada constante)
+altura2_const_euler = [0.0]     # Altura inicial en tanque 2 (Euler, entrada constante)
+altura1_const_rk = [1000.0]    # Altura inicial en tanque 1 (Runge-Kutta, entrada constante)
+altura2_const_rk = [0.0]       # Altura inicial en tanque 2 (Runge-Kutta, entrada constante)
 flujo_constante = [20.0] * len(tiempo)  # Flujo constante de entrada
-caudal_salida_const = []       # Salida del segundo tanque (flujo constante)
+caudal_salida_const_euler = []   # Salida del segundo tanque (Euler, flujo constante)
+caudal_salida_const_rk = []     # Salida del segundo tanque (Runge-Kutta, flujo constante)
 
-altura1_var = [1000.0]         # Altura inicial en tanque 1 (entrada variable)
-altura2_var = [0.0]            # Altura inicial en tanque 2 (entrada variable)
-flujo_variable_total = []      # Flujo variable de entrada
-caudal_salida_var = []         # Salida del segundo tanque (flujo variable)
+# Entrada variable
+altura1_var_euler = [1000.0]    # Altura inicial en tanque 1 (Euler, entrada variable)
+altura2_var_euler = [0.0]       # Altura inicial en tanque 2 (Euler, entrada variable)
+altura1_var_rk = [1000.0]      # Altura inicial en tanque 1 (Runge-Kutta, entrada variable)
+altura2_var_rk = [0.0]         # Altura inicial en tanque 2 (Runge-Kutta, entrada variable)
+flujo_variable_total = []        # Flujo variable de entrada
+caudal_salida_var_euler = []     # Salida del segundo tanque (Euler, flujo variable)
+caudal_salida_var_rk = []       # Salida del segundo tanque (Runge-Kutta, flujo variable)
 
 # --- Función para el flujo de entrada variable ---
 def calcular_flujo_variable(t):
     return 10 + 10 * np.sin(0.1 * t)
+
+# --- Función para el flujo de entrada variable ---
+def calcular_flujo_variable_ejer_5(t):
+    return 20.0 + 10.0 * np.sin(0.2 * t)  # Como en Ejercicio 5
+
+# --- Derivadas del sistema ---
+def derivadas(t, h1, h2, flujo_entrada):
+    dh1_dt = (flujo_entrada(t) - coef_transferencia * h1) / area_tanque
+    dh2_dt = (coef_transferencia * h1 - coef_descarga * np.sqrt(max(0, gravedad * h2))) / area_tanque
+    return dh1_dt, dh2_dt
 
 # --- Simulación temporal ---
 for i in range(1, len(tiempo)):
@@ -33,72 +51,138 @@ for i in range(1, len(tiempo)):
     flujo_entrada_var = calcular_flujo_variable(tiempo[i-1])
     flujo_variable_total.append(flujo_entrada_var)
 
-    # --- Entrada constante ---
-    h1_anterior_c = altura1_const[-1]
-    h2_anterior_c = altura2_const[-1]
+    # --- Entrada constante Euler ---
+    h1_anterior_c_euler = altura1_const_euler[-1]
+    h2_anterior_c_euler = altura2_const_euler[-1]
     
     # Derivadas de las alturas (ecuaciones diferenciales)
-    derivada_h1_c = (1 / area_tanque) * (flujo_constante[i-1] - coef_transferencia * h1_anterior_c)
-    derivada_h2_c = (1 / area_tanque) * (coef_transferencia * h1_anterior_c - coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_c)))
+    derivada_h1_c_euler, derivada_h2_c_euler = derivadas(tiempo[i-1], h1_anterior_c_euler, h2_anterior_c_euler, lambda t: 20.0)
 
-    # Actualización de alturas
-    altura1_const.append(h1_anterior_c + paso_tiempo * derivada_h1_c)
-    altura2_const.append(h2_anterior_c + paso_tiempo * derivada_h2_c)
+    # Actualización de alturas Euler
+    altura1_const_euler.append(h1_anterior_c_euler + paso_tiempo * derivada_h1_c_euler)
+    altura2_const_euler.append(h2_anterior_c_euler + paso_tiempo * derivada_h2_c_euler)
 
-    # Caudal de salida para entrada constante
-    caudal_salida_const.append(coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_c)))
+    # Caudal de salida para entrada constante Euler
+    caudal_salida_const_euler.append(coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_c_euler)))
 
-    # --- Entrada variable ---
-    h1_anterior_v = altura1_var[-1]
-    h2_anterior_v = altura2_var[-1]
+    # --- Entrada constante (Runge-Kutta) ---
+    h1_anterior_c_rk = altura1_const_rk[-1]
+    h2_anterior_c_rk = altura2_const_rk[-1]
+    # k1
+    k1_h1_c, k1_h2_c = derivadas(tiempo[i-1], h1_anterior_c_rk, h2_anterior_c_rk, lambda t: 20.0)
+    # k2
+    k2_h1_c, k2_h2_c = derivadas(tiempo[i-1] + paso_tiempo/2, 
+                                 h1_anterior_c_rk + (paso_tiempo/2) * k1_h1_c, 
+                                 h2_anterior_c_rk + (paso_tiempo/2) * k1_h2_c, 
+                                 lambda t: 20.0)
+    # k3
+    k3_h1_c, k3_h2_c = derivadas(tiempo[i-1] + paso_tiempo/2, 
+                                 h1_anterior_c_rk + (paso_tiempo/2) * k2_h1_c, 
+                                 h2_anterior_c_rk + (paso_tiempo/2) * k2_h2_c, 
+                                 lambda t: 20.0)
+    # k4
+    k4_h1_c, k4_h2_c = derivadas(tiempo[i-1] + paso_tiempo, 
+                                 h1_anterior_c_rk + paso_tiempo * k3_h1_c, 
+                                 h2_anterior_c_rk + paso_tiempo * k3_h2_c, 
+                                 lambda t: 20.0)
+    # Actualización
+    h1_new_c_rk = h1_anterior_c_rk + (paso_tiempo/6) * (k1_h1_c + 2*k2_h1_c + 2*k3_h1_c + k4_h1_c)
+    h2_new_c_rk = h2_anterior_c_rk + (paso_tiempo/6) * (k1_h2_c + 2*k2_h2_c + 2*k3_h2_c + k4_h2_c)
+    altura1_const_rk.append(max(0, h1_new_c_rk))
+    altura2_const_rk.append(max(0, h2_new_c_rk))
+    caudal_salida_const_rk.append(coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_c_rk)))
 
-    derivada_h1_v = (1 / area_tanque) * (flujo_entrada_var - coef_transferencia * h1_anterior_v)
-    derivada_h2_v = (1 / area_tanque) * (coef_transferencia * h1_anterior_v - coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_v)))
+    # --- Entrada variable Euler ---
+    h1_anterior_v_euler = altura1_var_euler[-1]
+    h2_anterior_v_euler = altura1_var_euler[-1]
 
-    altura1_var.append(h1_anterior_v + paso_tiempo * derivada_h1_v)
-    altura2_var.append(h2_anterior_v + paso_tiempo * derivada_h2_v)
+    derivada_h1_v_euler, derivada_h2_v_euler = derivadas(tiempo[i-1], h1_anterior_v_euler, h2_anterior_v_euler, calcular_flujo_variable)
 
-    # Caudal de salida para entrada variable
-    caudal_salida_var.append(coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_v)))
+    altura1_var_euler.append(h1_anterior_v_euler + paso_tiempo * derivada_h1_v_euler)
+    altura2_var_euler.append(h2_anterior_v_euler + paso_tiempo * derivada_h2_v_euler)
+
+    # Caudal de salida para entrada variable Euler
+    caudal_salida_var_euler.append(coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_v_euler)))
+
+    # --- Entrada variable (Runge-Kutta) ---
+    h1_anterior_v_rk = altura1_var_rk[-1]
+    h2_anterior_v_rk = altura2_var_rk[-1]
+    # k1
+    k1_h1_v, k1_h2_v = derivadas(tiempo[i-1], h1_anterior_v_rk, h2_anterior_v_rk, calcular_flujo_variable)
+    # k2
+    k2_h1_v, k2_h2_v = derivadas(tiempo[i-1] + paso_tiempo/2, 
+                                 h1_anterior_v_rk + (paso_tiempo/2) * k1_h1_v, 
+                                 h2_anterior_v_rk + (paso_tiempo/2) * k1_h2_v, 
+                                 calcular_flujo_variable)
+    # k3
+    k3_h1_v, k3_h2_v = derivadas(tiempo[i-1] + paso_tiempo/2, 
+                                 h1_anterior_v_rk + (paso_tiempo/2) * k2_h1_v, 
+                                 h2_anterior_v_rk + (paso_tiempo/2) * k2_h2_v, 
+                                 calcular_flujo_variable)
+    # k4
+    k4_h1_v, k4_h2_v = derivadas(tiempo[i-1] + paso_tiempo, 
+                                 h1_anterior_v_rk + paso_tiempo * k3_h1_v, 
+                                 h2_anterior_v_rk + paso_tiempo * k3_h2_v, 
+                                 calcular_flujo_variable)
+    # Actualización
+    h1_new_v_rk = h1_anterior_v_rk + (paso_tiempo/6) * (k1_h1_v + 2*k2_h1_v + 2*k3_h1_v + k4_h1_v)
+    h2_new_v_rk = h2_anterior_v_rk + (paso_tiempo/6) * (k1_h2_v + 2*k2_h2_v + 2*k3_h2_v + k4_h2_v)
+    altura1_var_rk.append(max(0, h1_new_v_rk))
+    altura2_var_rk.append(max(0, h2_new_v_rk))
+    caudal_salida_var_rk.append(coef_descarga * np.sqrt(max(0, gravedad * h2_anterior_v_rk)))
 
 # Agregar último valor de flujo y caudal para cerrar los arrays
 flujo_variable_total.append(calcular_flujo_variable(tiempo[-1]))
-caudal_salida_var.append(coef_descarga * np.sqrt(max(0, gravedad * altura2_var[-1])))
-caudal_salida_const.append(coef_descarga * np.sqrt(max(0, gravedad * altura2_const[-1])))
+caudal_salida_var_euler.append(coef_descarga * np.sqrt(max(0, gravedad * altura2_var_euler[-1])))
+caudal_salida_const_euler.append(coef_descarga * np.sqrt(max(0, gravedad * altura2_const_euler[-1])))
+caudal_salida_var_rk.append(coef_descarga * np.sqrt(max(0, gravedad * altura2_var_rk[-1])))
+caudal_salida_const_rk.append(coef_descarga * np.sqrt(max(0, gravedad * altura2_const_rk[-1])))
 
 # --- Gráficos ---
 fig, axs = plt.subplots(2, 2, figsize=(12, 8))
 fig.suptitle('Simulación de Tanques Acoplados', fontsize=16)
 
 # Altura h1(t)
-axs[0, 0].plot(tiempo, altura1_const, label='Entrada Constante')
-axs[0, 0].plot(tiempo, altura1_var, label='Entrada Variable')
+axs[0, 0].plot(tiempo, altura1_const_euler, label='Constante Euler', linestyle='--')
+axs[0, 0].plot(tiempo, altura1_const_rk, label='Constante Runge-Kutta')
+axs[0, 0].plot(tiempo, altura1_var_euler, label='Variable Euler', linestyle='--')
+axs[0, 0].plot(tiempo, altura1_var_rk, label='Variable Runge-Kutta')
 axs[0, 0].set_title('Altura del Tanque 1 (h1)')
+axs[0, 0].set_xlabel('Tiempo (s)')
+axs[0, 0].set_ylabel('Altura (m)')
 axs[0, 0].legend()
+axs[0, 0].grid(True)
 
 # Altura h2(t)
-axs[0, 1].plot(tiempo, altura2_const, label='Entrada Constante')
-axs[0, 1].plot(tiempo, altura2_var, label='Entrada Variable')
+axs[0, 1].plot(tiempo, altura2_const_euler, label='Constante Euler', linestyle='--')
+axs[0, 1].plot(tiempo, altura2_const_rk, label='Constante Runge-Kutta')
+axs[0, 1].plot(tiempo, altura2_var_euler, label='Variable Euler', linestyle='--')
+axs[0, 1].plot(tiempo, altura2_var_rk, label='Variable Runge-Kutta')
 axs[0, 1].set_title('Altura del Tanque 2 (h2)')
+axs[0, 1].set_xlabel('Tiempo (s)')
+axs[0, 1].set_ylabel('Altura (m)')
 axs[0, 1].legend()
+axs[0, 1].grid(True)
 
 # Flujo de entrada
 axs[1, 0].plot(tiempo, flujo_constante, label='Constante')
 axs[1, 0].plot(tiempo, flujo_variable_total, label='Variable')
 axs[1, 0].set_title('Flujo de Entrada F(t)')
+axs[1, 0].set_xlabel('Tiempo (s)')
+axs[1, 0].set_ylabel('Flujo (m³/s)')
 axs[1, 0].legend()
+axs[1, 0].grid(True)
 
 # Caudal de salida
-axs[1, 1].plot(tiempo, caudal_salida_const, label='Constante')
-axs[1, 1].plot(tiempo, caudal_salida_var, label='Variable')
+axs[1, 1].plot(tiempo, caudal_salida_const_euler, label='Constante Euler', linestyle='--')
+axs[1, 1].plot(tiempo, caudal_salida_const_rk, label='Constante Runge-Kutta')
+axs[1, 1].plot(tiempo, caudal_salida_var_euler, label='Variable Euler', linestyle='--')
+axs[1, 1].plot(tiempo, caudal_salida_var_rk, label='Variable Runge-Kutta')
 axs[1, 1].set_title('Caudal de Salida Q(t)')
+axs[1, 1].set_xlabel('Tiempo (s)')
+axs[1, 1].set_ylabel('Flujo (m³/s)')
 axs[1, 1].legend()
-
-# Etiquetas y formato
-for ax in axs.flat:
-    ax.set_xlabel('Tiempo (s)')
-    ax.set_ylabel('Valor')
-    ax.grid(True)
+axs[1, 1].grid(True)
 
 plt.tight_layout()
 plt.show()
